be546b2eb5894b8e62bc48ee0b500ce8c54d7ce6
kritayasa-lab <kritaya.sa@gmail.com>
2026-09-11 11:51:54 +0700

    Update integrations.ts

--- hayvia/config/integrations.ts
+++ hayvia/config/integrations.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyeTEKDpjArDOZTtBGmvJAIbpFWCqg31JDNNEF1dn7MnYZxnAHI4oBeBhnEzPGcqS-ciw/exec";
+  "https://script.google.com/macros/s/AKfycbwN4pDJYMviRDaXvvLt8FpfDm7fi4tiXs0HSDseGUTEz4JRvvpox2Bn_MvAU8NyJWg6QQ/exec";
